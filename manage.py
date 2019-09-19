0d7806b41de67c0f38ae76d4c381ae16e51b88a1
moharick <moharick@gmail.com>
2019-09-19 17:53:13 +0300

    Add manager commands.

--- manage.py
+++ manage.py
@@ -1,10 +1,26 @@
-from flask_script import Manager , Server
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+from app import create_app,db
+from app.models import User
 
-from app import create_app
-app=create_app()
+app = create_app('development')
 
-manager =  Manager(app)
-manager.add_command('runserver',Server(use_debugger=True))
+manager = Manager(app)
+migrate = Migrate(app,db)
 
-if __name__=="__main__":
+manager.add_command('db',MigrateCommand)
+manager.add_command('run',Server(use_debugger=True))
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+if __name__ == "__main__":
     manager.run()
