fbe5df60720e9afb741d23d82fa811538cb5a146
moharick <moharick@gmail.com>
2019-09-19 17:52:20 +0300

    Add configurations.

--- config.py
+++ config.py
@@ -1,2 +1,53 @@
+import os
+
 class Config:
-    DEBUG=True
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = '12323344ew'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'PITCHERS'
+    SENDER_EMAIL = 'moharick@gmail.com'
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:zuhura18@localhost/pitches'
+
+
+
+
+class ProdConfig(Config):
+    '''
+    Pruduction configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:zuhura18@localhost/pitches'
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+}
